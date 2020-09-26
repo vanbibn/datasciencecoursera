ff3854c583d751cfd0b8300ecc53e63abdf0a43c
Nathan Van Bibber <nvanbibber@mail.usf.edu>
2020-09-26 00:26:46 -0400

    Finalize census-app

--- Shiny/census-app/app.R
+++ Shiny/census-app/app.R
@@ -1,5 +1,11 @@
 library(shiny)
 
+source("helpers.R")
+counties <- readRDS("data/counties.rds")
+library(maps)
+library(mapproj)
+
+
 # Define the UI
 ui <- fluidPage(
     # Title the page
@@ -28,24 +34,43 @@ ui <- fluidPage(
         
         # Design the main panel
         mainPanel(
-            #p("You have selected Percent White")
-            textOutput("selected_var"),
-            textOutput("selected_range")
-            # add plot?
+            # textOutput("selected_var"),
+            # textOutput("selected_range"),
+            
+            # add map plot
+            plotOutput("map")
         )
     )
 )
 
 # Define the server logic
 server <- function(input, output) {
-    # var
-    # range
-    output$selected_var <- renderText({
-        paste("You have selected", input$var)
-    })
-    output$selected_range <-  renderText({
-        paste("You have chosen a range that goes from", 
-              input$range[1], "to", input$range[2])
+    # output$selected_var <- renderText({
+    #     paste("You have selected", input$var)
+    # })
+    # output$selected_range <-  renderText({
+    #     paste("You have chosen a range that goes from", 
+    #           input$range[1], "to", input$range[2])
+    # })
+    
+    # create the map object
+    output$map <- renderPlot({
+        data <- switch(input$var, 
+                       "Percent White" = counties$white,
+                       "Percent Black" = counties$black,
+                       "Percent Hispanic" = counties$hispanic,
+                       "Percent Asian" = counties$asian)
+        color <- switch(input$var, 
+                       "Percent White" = "darkgreen",
+                       "Percent Black" = "black",
+                       "Percent Hispanic" = "darkorange",
+                       "Percent Asian" = "purple")
+        
+        percent_map(var = data,
+                    color = color, 
+                    legend.title = input$var,
+                    min = input$range[1],
+                    max = input$range[2])
     })
 }
 
